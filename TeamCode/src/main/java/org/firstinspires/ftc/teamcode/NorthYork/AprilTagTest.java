3959e8d893e5b55bafa192a8f032104656148d1f
J-Dawggg <jadon.hui27@ycdsbk12.ca>
2026-04-10 09:13:42 -0400

    added @Teleop to AprilTagTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NorthYork/AprilTagTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NorthYork/AprilTagTest.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.NorthYork;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.NorthYork.mechanisms.Camera;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
+@TeleOp
 public class AprilTagTest extends OpMode {
     Camera camera = new Camera();
 
@@ -19,7 +21,7 @@ public class AprilTagTest extends OpMode {
 
         AprilTagDetection id20 = camera.getTagBySpecificId(20);
         camera.displayDetectionTelemetry(id20);
-        telemetry.addData("id20 String", id20.toString());
+        //telemetry.addData("id20 String", id20.toString());
      }
 
 }
